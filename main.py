f9a6eb1e116230065b9190f4e8df227123b83a4f
rayantoniel <antonielray5@gmail.com>
2023-11-17 21:38:52 -0300

    de NOVO

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from cliente import Cliente
+from Cliente import Cliente
 
 cliente1 = Cliente('Izabel', 12, 85296374112, 8569741235)
 cliente1.exibir()
 cliente1.cadastrar()
 
 
-from estoque import Produto
+from Estoque import Produto
 
 produto1 = Produto('Manga', 12, 4.50)
 produto1.cadastrar_produto()
 produto1.exibir_produto()
+
