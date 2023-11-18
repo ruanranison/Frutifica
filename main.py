c28655d8a5278e80140bca1de90a4f4922c2cc38
ruanranison <86027160+ruanranison@users.noreply.github.com>
2023-11-17 21:00:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from cliente import Cliente
+# from cliente import Cliente
 
-cliente1 = Cliente('Izabel', 12, 85296374112, 8569741235)
-cliente1.exibir()
-cliente1.cadastrar()
+# cliente1 = Cliente('Izabel', 12, 85296374112, 8569741235)
+# cliente1.exibir()
+# cliente1.cadastrar()
 
-from estoque import Estoque
-from estoque import Produto
+# from estoque import Estoque
+# from estoque import Produto
 
-estoque = Estoque()
+# estoque = Estoque()
 
-pr1 = Produto("Melancia", 20, 27.99)
-pr2 = Produto("Maçã", 50, 14.98)
+# pr1 = Produto("Melancia", 20, 27.99)
+# pr2 = Produto("Maçã", 50, 14.98)
 
-estoque.add_produto(pr1)
-estoque.add_produto(pr2)
+# estoque.add_produto(pr1)
+# estoque.add_produto(pr2)
 
-estoque.exibir_estoque()
+# estoque.exibir_estoque()
 
