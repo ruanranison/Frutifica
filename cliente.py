78ed02419b74b6245fd2952a0e9d8e90b2df364b
Ruan Ranison <rrcs1@aluno.ifal.edu.br>
2023-11-18 17:51:43 -0300

    cliente

--- cliente.py
+++ cliente.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Cliente:
     def __init__ (self, nome, idade, cpf, numero_telefone):
         self.nome = nome
@@ -6,20 +11,89 @@ class Cliente:
         self.numero_telefone = numero_telefone
 
     def exibir(self):
-        print(f"Nome: {self.nome}, Idade: {self.idade}, CPF: {self.idade}, Número de Telefone {self.numero_telefone}")
+        print(f"\nNome: {self.nome}\tIdade: {self.idade}\nCPF: {self.cpf}\t\tNúmero de Telefone: {self.numero_telefone}\n\n")
     
-    def cadastrar(self):
-        self.nome = input('Digite o nome do cliente: ')
-        self.idade = int(input('Digite a idade do cliente: '))
-        self.cpf = int(input('Digite o CPF do cliente: '))
-        self.numero_telefone = int(input('Digite o número de telefone: '))
-        resposta = input(f'Os dados do novo cliente são (digite SIM para confirmar e NAO para reiniciar): \nNome: {self.nome},\nIdade: {self.idade},\nCPF: {self.idade},\nNúmero de Telefone:  {self.numero_telefone} \n')
+    def cadastrar():
+        nome = input('Digite o nome do cliente: ')
+        idade = input('Digite a idade do cliente: ')
+        cpf = input('Digite o CPF do cliente: ')
+        numero_telefone = input('Digite o número de telefone: ')
+        resposta = input(f'Os dados do novo cliente são (digite SIM para confirmar e NAO para reiniciar): \nNome: {nome},\nIdade: {idade},\nCPF: {cpf},\nNúmero de Telefone:  {numero_telefone} \n')
 
         if resposta == "SIM":
-            print('Feto, coloque no arquivo')
+            c = open(os.getenv("Cliente"), "a")
+            c.write("\n"+nome+","+idade+","+cpf+","+numero_telefone)
+            c.close()
+            print("Cliente cadastrado!")
+            
         else:
-            Cliente.cadastrar(self)
-
+            Cliente.cadastrar()
+            
+    def consulta():
+        lista = []
+        c = open(os.getenv("Cliente"), "r")
+        for i in c:
+            cliente = i.replace("\n", "")
+            lista.append(cliente)
+        cpf_consulta = input('Digite o CPF do cliente para a consulta: ')
+        for i in lista:
+            if(cpf_consulta == i.split(",")[2]):
+                nome, idade, cpf, numero_telefone = i.split(",")
+                cliente = Cliente(nome=nome, idade=idade, cpf=cpf, numero_telefone=numero_telefone)
+                cliente.exibir()
+            else:
+                print("Dados incorretos!")
+                opcao = input("1 - Menu\t2 - Inserir dados novamente")
+                if opcao == 1: 
+                    break
+                else: 
+                    Cliente.consulta()
+        c.close()
+        
+    def deletar():
+        c = open(os.getenv("Cliente"), "r")
+        cpf_consulta = input('Digite o CPF do cliente para a consulta: ')
+        c = c.readlines()
+        for i in c:
+            if(cpf_consulta == i.split(",")[2]):
+                c.remove(i)
+                w = open(os.getenv("Cliente"), "w")
+                w.writelines(c)
+                print("Cliente deletado!\n")
+            else:
+                print("Dados incorretos!")
+                opcao = input("1 - Menu\t2 - Inserir dados novamente")
+                if opcao == 1: 
+                    break
+                else: 
+                    Cliente.deletar()
+        w.close()        
     
-
-
+    def alterar():
+        c = open(os.getenv("Cliente"), "r")
+        cpf_consulta = input('Digite o CPF do cliente para a consulta: ')
+        c = c.readlines()
+        for i in c:
+            if(cpf_consulta == i.split(",")[2]):
+                nome = input('Digite o nome do cliente: ')
+                idade = input('Digite a idade do cliente: ')
+                numero_telefone = input('Digite o número de telefone: ')
+                resposta = input(f'Os dados do cliente são (digite SIM para confirmar e NAO para reiniciar): \nNome: {nome},\nIdade: {idade},\nCPF: {cpf_consulta},\nNúmero de Telefone:  {numero_telefone} \n')
+                if resposta == "SIM":
+                    c.remove(i)
+                    alterado = nome+","+idade+","+cpf_consulta+","+numero_telefone
+                    c.append(alterado)
+                    w = open(os.getenv("Cliente"), "w")
+                    w.writelines(c)
+                    print("Cliente alterado!\n\n")
+                else:
+                    Cliente.alterar()
+                
+            else:
+                print("Dados incorretos!")
+                opcao = input("1 - Menu\t2 - Inserir dados novamente")
+                if opcao == 1: 
+                    break
+                else: 
+                    Cliente.alterar()
+        w.close()        
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-EstoqueProduto="db/EstoqueProduto.txt"
-Cliente="db/Cliente.txt"
-Caixa="db/Caixa.txt"
-ClienteProduto="db/ClienteProduto.txt"
-admin = open("db/admin.txt", 'r')
-admin = admin.readlines()
-LOGIN = admin[0].replace("\n", "")
-PASSWORD = admin[1].replace("\n", "")
+import os
+from dotenv import load_dotenv
+from cliente import Cliente
 
-def main():
-    print("\n\n\t\tFRUTIFICA\n\n")
-    login = input("Login: ")
-    senha = input("Senha: ")
+load_dotenv()
+Cliente.cadastrar()
+class Main():
+    def main():
+        print("\n\n\t\tFRUTIFICA\n\n")
+        login = input("Login: ")
+        senha = input("Senha: ")
 
-    if(login == LOGIN and senha == PASSWORD):
-        print("Usuário logado!\n\n\n")
-        print("\t\tCategorias\n")
-        int(input("1 - Estoque\t\t\t2 - Cliente\n3 - Caixa\t\t\t4 - Relatórios"))
-    else:
-        print("Usuário e/ou senha incorreta!")
-        main()
-
-main()
+        if(login == os.getenv("ADMIN") and senha == os.getenv("SENHA")):
+            print("Usuário logado!\n\n\n")
+            print("\t\tCategorias\n")
+            opcao = int(input("1 - Estoque\t\t\t2 - Cliente\n3 - Caixa\t\t\t4 - Relatórios\n\t\t\t5 - Sair"))
+            while opcao != 5:
+                if opcao == 1:
+                    # menu estoque
+                    break
+                elif opcao == 2:
+                    # menu cliente
+                    break
+                elif opcao == 3:
+                    # menu caixa
+                    break
+                elif opcao == 4:
+                    # menu relatorios
+                    break
+        else:
+            print("Usuário e/ou senha incorreta!")
+            Main.main()
 
 
 
@@ -43,3 +52,21 @@ main()
 # faturamento diário*
 
 
+# from cliente import Cliente
+
+# cliente1 = Cliente('Izabel', 12, 85296374112, 8569741235)
+# cliente1.exibir()
+# cliente1.cadastrar()
+
+# from estoque import Estoque
+# from estoque import Produto
+
+# estoque = Estoque()
+
+# pr1 = Produto("Melancia", 20, 27.99)
+# pr2 = Produto("Maçã", 50, 14.98)
+
+# estoque.add_produto(pr1)
+# estoque.add_produto(pr2)
+
+# estoque.exibir_estoque()
