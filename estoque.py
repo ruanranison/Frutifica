4ed9c8df08f8cb8fbe56ae046a6287529f1a545c
rayantoniel <antonielray5@gmail.com>
2023-11-17 21:14:52 -0300

    vai menino

--- estoque.py
+++ estoque.py
@@ -4,4 +4,16 @@ class Produto:
         self.quantidade = quantidade
         self.preco = preco
 
+    def cadastrar_produto(self):
+        self.nome = input('Digite o nome do produto: ')
+        self.quantidade = float(input('Digite a quantidade do produto: '))
+        self.preco = float(input('Digite o valor do produto: R$ '))
+        respostap = input(f'Para confirmar os dados, digite SIM, para REINICIAR, digite NAO \nNome: {self.nome} \nQuantidade: {self.quantidade} \nPreço: R${self.preco} \n')
+            
+        if respostap == 'SIM':
+            print('Feto, coloque no arquivo')
+        else:
+            Produto.cadastrar_produto(self)
     
+    def exibir_produto(self):
+        print(f'Nome: {self.nome}, Quantidade: {self.quantidade}, Preço: R${self.preco} ')
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# from cliente import Cliente
+from cliente import Cliente
 
-# cliente1 = Cliente('Izabel', 12, 85296374112, 8569741235)
-# cliente1.exibir()
-# cliente1.cadastrar()
+cliente1 = Cliente('Izabel', 12, 85296374112, 8569741235)
+cliente1.exibir()
+cliente1.cadastrar()
 
 
 from estoque import Produto
+
 produto1 = Produto('Manga', 12, 4.50)
 produto1.cadastrar_produto()
 produto1.exibir_produto()
